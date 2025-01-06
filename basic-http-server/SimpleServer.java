ed3d5053d30d7d222ca650c6fdea31da99c4d1cc
yleeyilin <yiiiliiinn@gmail.com>
2025-01-06 22:11:00 +0100

    Add simple server

---
+++ basic-http-server/SimpleServer.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SimpleServer {
+    public static void main(String[] args) throws IOException {
+        int port = 8080; // binded port
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Server running on port " + port);
+            while (true) {
+                Socket clientSocket = serverSocket.accept(); 
+                handleClient(clientSocket); 
+            }
+        }
+    }
+
+    private static void handleClient(Socket socket) throws IOException {
+        try (InputStream inputStream = socket.getInputStream();
+             OutputStream outputStream = socket.getOutputStream()) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String requestLine = reader.readLine(); // Reads GET / HTTP/1.1
+            System.out.println("Request: " + requestLine);
+
+            String response = "HTTP/1.1 200 OK\n" +
+                   "Content-Type: text/plain\n" +
+                   "Content-Length: 13\n"+
+                   "\n" +
+                   "Hello, World!\n";
+
+            outputStream.write(response.getBytes()); 
+        } finally {
+            socket.close(); 
+        }
+    }
+}
