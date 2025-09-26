68125c4d103dd5d338e2d3862739f0eead271b8f
yleeyilin <yiiiliiinn@gmail.com>
2025-09-26 20:04:18 +0800

    Add cum sum gen

---
+++ python/cum_sum_gen.py
@@ -0,0 +1,16 @@
+from typing import Generator
+
+def cum_sum() -> Generator[int, int, None]:
+    total = 0
+
+    while True:
+        new_val = yield total
+        total += new_val
+
+if __name__ == "__main__":
+    cs_gen = cum_sum()
+    next(cs_gen) # give us the total 
+    while True:
+        to_add = input("Value to add: ")
+        total = cs_gen.send(int(to_add)) # pass it to the yield variable 
+        print(f"curr sum is: {total}")
